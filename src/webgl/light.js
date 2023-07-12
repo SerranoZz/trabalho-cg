7c09072014ccce349c0144b918697e8376e8d1c2
Lucas Serrano <lucas_serrano@id.uff.br>
2023-07-12 16:20:06 -0300

    testando

--- src/webgl/light.js
+++ src/webgl/light.js
@@ -2,19 +2,19 @@ export default class Light {
   constructor(position, color) {
     this.pos = vec4.fromValues(position[0], position[1], position[2], 1.0);
 
-    this.amb_c = vec4.fromValues(color[0], color[1], color[2], 1.0);
+    this.amb_c0 = vec4.fromValues(color[0], color[1], color[2], 1.0);
     this.amb_k = 0.4;
 
-    this.dif_c = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
+    this.dif_c0 = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
     this.dif_k = 0.5;
 
-    this.esp_c = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
+    this.esp_c0 = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
     this.esp_k = 0.4;
     this.esp_p = 5.0;
   }
 
   createUniforms(gl, program){
-    const posLoc = gl.getUniformLocation(program, "light_pos");
+    const posLoc = gl.getUniformLocation(program, "light_pos0");
     gl.uniform4fv(posLoc, this.pos);
 
     const ambCLoc = gl.getUniformLocation(program, "light_amb_c");
@@ -22,12 +22,12 @@ export default class Light {
     const ambKLoc = gl.getUniformLocation(program, "light_amb_k")
     gl.uniform1f(ambKLoc, this.amb_k);
 
-    const difCLoc = gl.getUniformLocation(program, "light_dif_c");
+    const difCLoc = gl.getUniformLocation(program, "light_dif_c0");
     gl.uniform4fv(difCLoc, this.dif_c);
     const difKLoc = gl.getUniformLocation(program, "light_dif_k")
     gl.uniform1f(difKLoc, this.dif_k);
 
-    const espCLoc = gl.getUniformLocation(program, "light_esp_c");
+    const espCLoc = gl.getUniformLocation(program, "light_esp_c0");
     gl.uniform4fv(espCLoc, this.pos);
     const espKLoc = gl.getUniformLocation(program, "light_esp_k")
     gl.uniform1f(espKLoc, this.esp_k);
